6ba2e0c84e2e1299d81103b48f11b9985e3b8358
Akash Agrawal <akash.agrawal@radius.ai>
2025-03-30 23:31:57 +0530

    Simplify flow test

--- app/src/main/java/com/example/myapplication/LongPollingHelper.kt
+++ app/src/main/java/com/example/myapplication/LongPollingHelper.kt
@@ -1,17 +1,29 @@
 package com.example.myapplication
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import rai.blink.model.ScanResponseData
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import rai.blink.api.DefaultApi
+import rai.blink.infrastructure.HttpResponse
 
-object LongPollingHelper {
-    fun longPollingFlow(apiInstance: DefaultApi): Flow<ScanResponseData> = flow {
-        while (true) {
-            val scanData: ScanResponseData = apiInstance.scanGet().body()
-            emit(scanData)
+fun smartsense(): Flow<HttpResponse<ScanResponseData>> = flow {
+    val client = HttpClient(OkHttp) {
+        install(ContentNegotiation) {
+            json()
         }
-    }.flowOn(Dispatchers.IO)
-}
+    }
+
+    val apiInstance = DefaultApi("http://192.168.29.19:8000", client)
+
+    while (true) {
+        emit(apiInstance.scanGet())
+        delay(10)
+    }
+}.flowOn(Dispatchers.IO)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -13,8 +12,6 @@ import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel: ScanItemsViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,9 +20,9 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.scan.collect { data ->
-                    Log.d("coro", "Received: $data")
-                    val adapter = ItemsAdapter(data.scanData)
+                smartsense().collect { data ->
+                    Log.d("coro", "Received: ${data.body()}")
+                    val adapter = ItemsAdapter(data.body().scanData)
                     recyclerView.adapter = adapter
                 }
             }
