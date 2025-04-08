55a190e4ca0e22adca00273dd27ee29a0d554a6a
Akash Agrawal <akash.agrawal@radius.ai>
2025-04-08 14:58:43 +0530

    Updated with the latest changes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,12 +8,9 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.launch
-import rai.blink.api.DefaultApi
+import rai.blink.api.BlinkApi
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,19 +20,14 @@ class MainActivity : AppCompatActivity() {
         Log.d("main", "onCreate: Init")
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        val client = HttpClient(OkHttp) {
-            install(ContentNegotiation) {
-                json()
-            }
-        }
 
-        val apiInstance = DefaultApi("http://192.168.29.19:8000", client)
+        val apiInstance = BlinkApi("10.206.160.145:8000", true)
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 apiInstance.smartsense().collect { data ->
-                    Log.d("coro", "Received: ${data.body()}")
-                    val adapter = ItemsAdapter(data.body().scanData)
+                    Log.d("loop", "Received: $data")
+                    val adapter = ItemsAdapter(data.scanData)
                     recyclerView.adapter = adapter
                 }
             }
